1360f07e8b9272df39bb2432785c1f49394cb399
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2023-11-27 21:37:33 +0700

    1. Variable declarations

---
+++ cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades" // := only used for assigning new variable, to define the var data type
+	card = "Five of Diamonds"
+
+	fmt.Println(card)	
+}
