21875da074fa6db94476a9829e651cff9f9e442f
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2023-11-28 21:31:52 +0700

    multiple return value

--- cards/deck.go
+++ cards/deck.go
@@ -21,6 +21,10 @@ func newDeck() deck {
 	return cards
 }
 
+func deal(d deck, handSize int) (deck, deck){
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
