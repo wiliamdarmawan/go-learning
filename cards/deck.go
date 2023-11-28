b6f5e60d776a42e124b79994b0aa0e2588fbf038
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2023-11-28 21:23:31 +0700

    create new deck

--- cards/deck.go
+++ cards/deck.go
@@ -7,6 +7,20 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
