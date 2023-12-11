9f5e92530ad86c4d6398568c8392cb8b70c7fb67
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2023-12-11 20:36:36 +0700

    iterating over maps

--- map/main.go
+++ map/main.go
@@ -3,17 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
+	colors := map[string]string{
+		"red": "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+	printMap(colors)
+}
 
-	colors := make(map[int]string)
-	colors[10] = "#fffff"
-
-	delete(colors, 10)
-
-	// colors := map[string]string{
-	// 	"red": "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
