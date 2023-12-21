3b4ab470e57abb4c02a198bc04759365090498bb
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2023-12-21 20:39:32 +0700

    interfaces in practice 1

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -10,15 +14,11 @@ func main() {
 	 sb := spanishBot{}
 
 	 printGreeting(eb)
-	//  printGreeting(sb)
+	 printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (englishBot) getGreeting() string{
