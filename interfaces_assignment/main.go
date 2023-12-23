ab34fa29c2311abc5d270ae9154330dde489d124
wiliamdarmawan <generousbil@gmail.com>
2023-12-23 22:48:30 +0700

    interfaces assignment 1

---
+++ interfaces_assignment/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct{
+	height	float64
+	base	float64
+}
+
+type square struct{
+	sideLength	float64
+}
+
+func main() {
+	 s := square{
+		sideLength: 4,
+	 }
+
+	 t := triangle{
+		base: 3,
+		height: 15,
+	 }
+
+	printArea(s)
+	printArea(t)
+}
+
+func printArea(s shape){
+	fmt.Println(s.getArea())
+}
+
+func (s square) getArea() float64{
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64{
+	return 0.5 * t.base * t.height
+}
