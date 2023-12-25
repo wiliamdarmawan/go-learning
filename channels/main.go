1904f58990c9d74b9bb6ee7aa09e1c3eed40fef4
wiliamdarmawan <generousbil@gmail.com>
2023-12-25 21:31:02 +0700

    repeating routines

--- channels/main.go
+++ channels/main.go
@@ -20,8 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<- c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -30,10 +30,10 @@ func checkLink(link string, c chan string) {
 
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep its up"
+	c <- link
 }
